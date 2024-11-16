5c2448965c2d445bbe23ba3d35d7c7cca04e3174
deonna <deonnaowens3@yahoo.com>
2024-11-15 23:53:09 -0800

    changes made to model

--- model.py
+++ model.py
@@ -7,6 +7,9 @@ from tensorflow.keras.layers import LSTM, Dense, Dropout
 from tensorflow.keras.layers import Bidirectional
 from tensorflow.keras.layers import Conv1D, BatchNormalization
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.model_selection import cross_val_score
+from xgboost import plot_importance
 import tensorflow.keras.backend as K
 import os
 import matplotlib.pyplot as plt
@@ -18,7 +21,7 @@ This script implements a machine learning model using Long Short-Term Memory (LS
 The model takes a sequence of past monthly spending data as input and generates a single output representing the predicted spending for the next month.
 
 Main Components:
-1. MonthlySpendingPredictionModel Class:
+1. SpendingPredictionModel Class:
    - __init__: Initializes the model parameters, including sequence length and scalers for feature normalization.
    - load_and_preprocess_data: Loads and preprocesses user transaction data, normalizing continuous features and creating input-output sequences for the model.
    - _create_sequences: Generates sequences of input data and corresponding targets for training and prediction, based on a specified sequence length.
@@ -28,7 +31,7 @@ Main Components:
    - predict: Makes predictions on new input data and denormalizes the results to the original spending scale.
 
 2. Main Execution:
-   - Loads the dataset, initializes the MonthlySpendingPredictionModel, and preprocesses the data.
+   - Loads the dataset, initializes the SpendingPredictionModel, and preprocesses the data.
    - Splits the data into training, validation, and test sets.
    - Builds, trains, and evaluates the model, then makes predictions on a sample of test data.
 
@@ -41,10 +44,12 @@ Dependencies:
 
 """
 
+def mean_absolute_percentage_error(y_true, y_pred):
+    return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
 
 class SpendingPredictionModel:
     
-    def __init__(self, seq_length=50):
+    def __init__(self, seq_length=11):
         """
         Initializes the model with a sequence length for the LSTM.
 
@@ -67,65 +72,29 @@ class SpendingPredictionModel:
         Returns:
         - X, y: Processed input features and labels
         """
-        # Convert date to datetime and create monthly periods
-        data['date'] = pd.to_datetime(data['date']) # Converts date column to datatime object for sequential processing
-        data = data.sort_values(by=['user_id', 'date']) # Sorts user id and date in chronological order for sequential processing
 
-        data['day_of_week'] = data['date'].dt.dayofweek
-        data['day_of_month'] = data['date'].dt.day
-        data['month'] = data['date'].dt.month
-        data['year'] = data['date'].dt.year
-
-        self.amount_scaler.fit(data[['amount']])
-        data['amount'] = self.amount_scaler.transform(data[['amount']])
-        
-        # Normalize continuous features at the transaction level
-        numerical_columns = ['income', 'age', 'family_size']
-        data[numerical_columns] = self.scaler.fit_transform(data[numerical_columns])
-
-        
-        category_encoded = pd.get_dummies(data, columns=['category'], dtype='float32')
-        # print('CATEGORY DATA: ', category_encoded)
-        # exit()
-
-        data = pd.concat([data, category_encoded], axis=1)
-        data = data.loc[:, ~data.columns.duplicated()]
-        
-        # Update feature_columns to include the new one-hot encoded columns
-        category_columns = [col for col in category_encoded.columns if 'category_' in col]
-        # feature_columns = ['amount', 'income', 'age', 'family_size'] 
-
-        
-        # print('DATA COLUMNS: ', data.columns)
-        # exit()
-
-        # Define the feature columns you want to use in the model
-        feature_columns = ['amount', 'income', 'age', 'family_size', 'day_of_week', 'day_of_month', 'month', 'year'] + category_columns
-        print('FEATURE COLUMNS SIZE: ', len(feature_columns))
-
-        
         # Summarizes spending for each month for each user
-        # monthly_data = data.groupby(['user_id', 'year', 'month']).agg({
-        #     'amount': 'sum',
-        #     'income': 'first',
-        #     'family_size': 'first',
-        #     'age': 'first',
-        # }).reset_index()
+        monthly_data = data.groupby(['user_id', 'year', 'month']).agg({
+            'amount': 'sum',           # Sum total spending for each category within the month
+            'income': 'first',          # Keep income constant for the user
+            'family_size': 'first',      # Keep family size constant for the user
+            'age': 'first'               # Keep age constant for the user
+        }).reset_index()
 
         # Normalize 'amount' separately using amount_scaler
-        # monthly_data['amount'] = self.amount_scaler.fit_transform(monthly_data[['amount']])
+        monthly_data['amount'] = self.amount_scaler.fit_transform(monthly_data[['amount']])
 
         # Normalize the other continuous features using the main scaler
-        # monthly_data[['income', 'family_size', 'age']] = \
-        #     self.scaler.fit_transform(monthly_data[['income', 'family_size', 'age']])
+        monthly_data[['income', 'family_size', 'age']] = self.scaler.fit_transform(
+            monthly_data[['income', 'family_size', 'age']]
+        )
         
         # Create sequences and labels
-        # X, y = self._create_sequences(monthly_data)
-
-        X, y = self._create_sequences(data, feature_columns)
+        X, y = self._create_sequences(monthly_data)
+        
         return X, y
 
-    def _create_sequences(self, data, feature_columns):
+    def _create_sequences(self, data):
         """
         Creates sequences from the data for LSTM input.
 
@@ -137,13 +106,12 @@ class SpendingPredictionModel:
         """
         sequences = []
         targets = []
-    
         # We create sequences of 3 months length
         for user_id in data['user_id'].unique():
             user_data = data[data['user_id'] == user_id]
-            # This loop will allow us to stop when we can no longer create full sequences
+            # This loop will allow us to stop when we can no longer create full sequences (3 months + 1 month for target)
             for i in range(len(user_data) - self.seq_length):
-                seq = user_data[feature_columns].iloc[i:i + self.seq_length].values
+                seq = user_data[['amount', 'income', 'family_size', 'age']].iloc[i:i + self.seq_length].values
                 target = user_data['amount'].iloc[i + self.seq_length]
                 sequences.append(seq)
                 targets.append(target)
@@ -155,7 +123,7 @@ class SpendingPredictionModel:
         Builds and compiles the LSTM model.
 
         """
-        num_features = 17
+        num_features = 4
         self.model = Sequential()
 
         self.model.add(Conv1D(filters=64, kernel_size=2, activation='relu', input_shape=(self.seq_length, num_features)))
@@ -164,12 +132,12 @@ class SpendingPredictionModel:
         self.model.add(LSTM(128, return_sequences=True))
         self.model.add(Dropout(0.1))
         self.model.add(LSTM(64)) 
-        # self.model.add(Dropout(0.1))  
+        self.model.add(Dropout(0.1))  
         self.model.add(Dense(1))  
 
         self.model.compile(optimizer='adam', loss='mean_squared_error', metrics=['mae'])
 
-    def train_model(self, X_train, y_train, X_val, y_val, epochs=20, batch_size=128):
+    def train_model(self, X_train, y_train, X_val, y_val, epochs=20, batch_size=32):
         """
         Trains the LSTM model.
 
@@ -226,10 +194,10 @@ class SpendingPredictionModel:
 if __name__ == "__main__":
    
     data_dir = os.getcwd()
-    data_path = os.path.join(data_dir, 'data/synthetic_financial_data_test.csv')
+    data_path = os.path.join(data_dir, 'data/synthetic_financial_data_1.5M.csv')
     data = pd.read_csv(data_path)  
 
-    model = SpendingPredictionModel(seq_length=50)
+    model = SpendingPredictionModel(seq_length=11)
 
     X, y = model.load_and_preprocess_data(data)
 
@@ -239,7 +207,7 @@ if __name__ == "__main__":
     model.build_model()
     print(model.model.summary())
 
-    history = model.train_model(X_train, y_train, X_val, y_val, epochs=20, batch_size=128)
+    history = model.train_model(X_train, y_train, X_val, y_val, epochs=20, batch_size=32)
 
     loss, mae = model.evaluate_model(X_test, y_test)
 
@@ -263,7 +231,9 @@ if __name__ == "__main__":
     accuracy = np.mean(within_tolerance) * 100
 
     print(f"Accuracy within ±10% tolerance: {accuracy:.2f}%")
-    
+
+    print('MAPE: ', mean_absolute_percentage_error(y_test_denormalized, predictions_denormalized))
+
     # Plot predicted vs actual spending
     plt.figure(figsize=(10, 6))
     plt.plot(predictions_denormalized, label="Predicted Spending", linestyle="--", marker="o", color="blue")
